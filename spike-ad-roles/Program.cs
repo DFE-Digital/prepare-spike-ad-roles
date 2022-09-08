a2e098e40c61115cd2bc0375b05812014b7a630a
MartinWheelerMT <martin.wheeler@madetech.com>
2022-09-08 18:18:01 +0100

    Initial Commit

---
+++ spike-ad-roles/Program.cs
@@ -0,0 +1,14 @@
+﻿using spike_ad_roles;
+
+const string groupName = "PrepareForABDevTesting";
+
+var memberUserIds = await GraphManager.GetGroupMemberUserIdsAsync(groupName);
+
+Console.WriteLine();
+
+Console.WriteLine("Member Count: {0}", memberUserIds.Count());
+Console.WriteLine();
+
+//TODO: This will fail due to access issues
+var userDetails = await GraphManager.GetGroupMemberUserDetailsAsync(groupName);
+Console.WriteLine("Count: {0}", userDetails.Count());
